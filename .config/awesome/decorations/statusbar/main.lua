a27339bfcd6074907f1fe0f86b95bd216dd85de1
Leonardo Moura <leofmoura17@gmail.com>
2021-03-11 14:29:45 +0000

    Modified taglist to underline selected tag

--- .config/awesome/decorations/statusbar/main.lua
+++ .config/awesome/decorations/statusbar/main.lua
@@ -22,6 +22,14 @@ local separator = {
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
+local update_tag = function(item, tag, index)
+    local highlight = item:get_children_by_id('highlight')[1]
+    local bg = item:get_children_by_id('background_role')[1]
+
+    highlight.visible = tag.selected
+    bg.bg = beautiful.bg_normal
+end
+ 
 
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
@@ -44,7 +52,44 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = awful.widget.taglist {
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
+        buttons = taglist_buttons,
+        style = {
+            bg_focus = beautiful.bg_normal
+        },
+        widget_template = {
+            {
+                {
+                    nil,
+                    {
+                        id = "text_role",
+                        widget = wibox.widget.textbox,
+                    },
+                    {
+                        {
+                            {
+                                widget = wibox.widget.textbox
+                            },
+                            bg = beautiful.bg_focus,
+                            id = "highlight",
+                            widget = wibox.container.background
+                        },
+                        forced_height = 2,
+                        widget = wibox.container.background
+                    },
+                    layout  = wibox.layout.align.vertical
+                },
+                id = 'background_role',
+                widget = wibox.container.background,
+                bg = beautiful.bg_normal
+            },
+            widget = wibox.container.margin,
+            left = 3,
+            right = 3,
+
+            create_callback = update_tag,
+            
+            update_callback = update_tag
+        }
     }
 
     -- Create a tasklist widget
