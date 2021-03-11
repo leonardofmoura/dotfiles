f5fe1fd4624119c14a65065c7635d88057b620c4
Leonardo Moura <leofmoura17@gmail.com>
2021-03-11 14:52:17 +0000

    Changed terminal background and theme focus background

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -14,7 +14,7 @@ local theme = {}
 theme.font          = "sans 10"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#295e93"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
