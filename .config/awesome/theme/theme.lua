4e1993e3986320a8dbdcc5aec94a41abb90136d7
Leonardo Moura <leofmoura17@gmail.com>
2021-10-03 18:43:44 +0200

    changed wallpaper and added transparency to terminal

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "DejaVuSansMono 10"
+theme.font          = "DejaVuSansMono 11"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#295e93"
@@ -97,7 +97,8 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "~/Images/Wallpapers/wallhaven-wqqlox.jpg"
+-- theme.wallpaper = "~/Images/Wallpapers/wallhaven-wqqlox.jpg"
+theme.wallpaper = "~/Images/Wallpapers/flat-landscape-minimalism-trees-sunset-river-artwork-22750-3840x2160.jpeg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
