d7cdacc32d9b986f3951bd0a393683fc09adcd0a
leonardofmoura <leofmoura17@gmail.com>
2025-06-08 14:33:57 +0100

    add better markdown writing

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -23,69 +23,94 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        highlight = { enable = true},
+        indent = { enable = true },
+        sync_install = false,
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "css",
+          "diff",
+          "dockerfile",
+          "doxygen",
+          "go",
+          "groovy",
+          "html",
+          "java",
+          "javascript",
+          "jsdoc",
+          "json",
+          "jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "make",
+          "markdown",
+          "markdown_inline",
+          "printf",
+          "python",
+          "query",
+          "regex",
+          "rust",
+          "scss",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "xml",
+          "yaml",
+        }
+      })  
+    end
+  },
+
+  {
+    'echasnovski/mini.pairs',
+    version = false,
     opts = {
-      highlight = { enable = true},
-      indent = { enable = true },
-      sync_install = false,
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "css",
-        "diff",
-        "dockerfile",
-        "doxygen",
-        "go",
-        "groovy",
-        "html",
-        "java",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "scss",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      }
-    },
-
-    {
-      'echasnovski/mini.pairs',
-      version = false,
-      opts = {
-        skip_ts = {"string"}, -- do noy autopair inside string
-        skip_unbalanced = true, -- do not autopair when things are messy
-        markdown = true -- better deal with markdown
-      }
-    },
-
-    {
-      "lewis6991/gitsigns.nvim",
-      opts = {}
-    },
-
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      ---@module "ibl"
-      ---@type ibl.config
-      opts = {},
+      skip_ts = {"string"}, -- do noy autopair inside string
+      skip_unbalanced = true, -- do not autopair when things are messy
+      markdown = true -- better deal with markdown
     }
-  }
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {}
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
+    "folke/twilight.nvim",
+    opts = {}
+  },
+
+  {
+    "folke/zen-mode.nvim",
+    opts = {}
+  },
 }
+
