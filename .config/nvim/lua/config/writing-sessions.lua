19461a469809ca7eaac88fd5c437c624de2ba12d
leonardofmoura <leofmoura17@gmail.com>
2026-01-14 11:52:20 +0000

    improve statistics

--- .config/nvim/lua/config/writing-sessions.lua
+++ .config/nvim/lua/config/writing-sessions.lua
@@ -1,45 +1,107 @@
 local starting_words = nil
+local starting_time = nil
+local session_started = false
 
--- Start writing session
+
+
+local function format_time(seconds)
+  local hours = math.floor(seconds / 3600)
+  local mins = math.floor((seconds % 3600) / 60)
+  local secs = seconds % 60
+
+  local time_str = string.format("%02d:%02d:%02d", hours, mins, secs)
+
+  return time_str
+end
+
+
+local function calculate_wph(total_words, seconds)
+  local wph = 0
+
+  if seconds > 0 then
+    wph = math.floor((total_words / seconds) * 3600)
+  end
+  
+  return wph
+end
+
+
+local function get_session_state()
+  local current_words = vim.fn.wordcount().words
+  local current_time = os.time()
+
+  local total_words = current_words - starting_words
+  local total_time = current_time - starting_time
+
+  return total_words, total_time
+end
+
+
 local function  start_writing_session()
-  -- session already started: 
-  if starting_words ~= nil then 
+  if session_started then
     vim.notify("Session already started", vim.log.levels.INFO)
     return
   end
 
-
   starting_words = vim.fn.wordcount().words
+  starting_time = os.time()
+  session_started = true
+
   vim.notify("Writing Session Started", vim.log.levels.INFO)
 end
 
 
 local function terminate_writing_session()
-  if starting_words == nil then
-    vim.notify("Writing Session already active", vim.log.levels.INFO)
+  if  not session_started then
+    vim.notify("No Writing Session active", vim.log.levels.INFO)
     return
   end
   
-  local final_words = vim.fn.wordcount().words
-  local total = final_words - starting_words
+  local total_words, total_time = get_session_state()
 
+  local duration = format_time(total_time)
+  local wph = calculate_wph(total_words,total_time)
+
+  local session_info = string.format("󰴓 %d | 󰥔  %s |   %d", total_words, duration, wph)
+
+
+  -- reset session state
   starting_words = nil
+  starting_time = nil
+  session_started = false
 
-  vim.notify("Writing Session ended. Words Written: " .. (total), vim.log.levels.INFO)
+  vim.notify("Writing Session ended. Session info: " .. (session_info), vim.log.levels.INFO)
 end
 
 
+
+-- function used for lualine
 function GetSessionProgress()
-  if starting_words == nil  then
+  if not session_started  then
+    return "No Writing Session active"
+  end
+  
+  local total_words, total_time = get_session_state()
+  local duration = format_time(total_time)
+
+  return string.format("󰴓 %d | 󰥔  %s", total_words, duration)
+end
+
+
+-- function called in the command
+local function session_progress()
+  if not session_started  then
     return "No Writing Session active"
   end
 
-  return "Words Written: " .. (vim.fn.wordcount().words - starting_words)
-end
+  local total_words, total_time = get_session_state()
 
+  local duration = format_time(total_time)
+  local wph = calculate_wph(total_words,total_time)
 
-local function session_progress() 
-  vim.notify(GetSessionProgress(), vim.log.levels.INFO)
+  local progress = string.format("󰴓 %d | 󰥔  %s |   %d", total_words, duration, wph)
+
+  vim.notify(progress, vim.log.levels.INFO)
 end
 
 
